c386c34194b6b7301028941c1855f1735c87e66f
Mohit Saini <mailtomohit2002@gmail.com>
2023-09-03 01:50:15 +0530

    Added tokens applications access and refresh

--- backend/urls.py
+++ backend/urls.py
@@ -3,9 +3,11 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("rest_framework.urls")),
     # including application_name.urls
     
     # In Django's URL configuration, the namespace parameter is used to provide a unique identifier for a set of URLs defined within an included URL configuration. It's especially useful when you have multiple applications with their own sets of URLs and you want to avoid naming conflicts between them.
     path("products/", include("products.urls", namespace="products")),
+
+    path("auth/", include("authtokens.urls", namespace="authtokens")),
 ]
+ 
